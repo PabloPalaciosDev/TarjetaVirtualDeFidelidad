aa8de7acadccc812501d7a253afcaa87031c8354
Pablo Palacios <72522406+PabloPalaciosDev@users.noreply.github.com>
2024-11-14 09:19:55 -0500

    - Servicio de tarjetas de fidelidad listo, falta controlador

--- SistemaDeFidelidad/Services/ServiceTarjetaFidelidad.cs
+++ SistemaDeFidelidad/Services/ServiceTarjetaFidelidad.cs
@@ -6,30 +6,89 @@ namespace SistemaDeFidelidad.Services
     public class ServiceTarjetaFidelidad : IServices<TarjetaFidelidad>
     {
         private readonly IRepository<TarjetaFidelidad> _repository;
+        private readonly IRepository<ClienteParticipante> _clienteParticipante;
+        private readonly IRepository<DescuentosCliente> _descuentosCliente;
 
-        public ServiceTarjetaFidelidad(IRepository<TarjetaFidelidad> repository)
+        public ServiceTarjetaFidelidad(IRepository<TarjetaFidelidad> repository, IRepository<ClienteParticipante> clienteParticipante, IRepository<DescuentosCliente> descuentosCliente)
         {
             _repository = repository;
+            _clienteParticipante = clienteParticipante;
+            _descuentosCliente = descuentosCliente;
         }
 
-        public Task<ServiceResult> AddAsync(TarjetaFidelidad entity)
+        public async Task<ServiceResult> AddAsync(TarjetaFidelidad entity)
         {
-            throw new NotImplementedException();
+            //Obtener cliente
+            try
+            {
+                var clienteFound = await _clienteParticipante.GetByGuidAsync(entity.IdCliente, "IdCliente", c => c.Tarjetas, c => c.Descuentos);
+                if (clienteFound == null)
+                {
+                    return ServiceResult.FailureResult("Cliente no encontrado");
+                }
+
+                await _repository.AddAsync(entity);
+                await _repository.SaveAsync();
+
+                return ServiceResult.SuccessResult("Tarjeta creada con éxito");
+            }
+            catch (Exception e)
+            {
+                return ServiceResult.FailureResult(e.Message);
+            }
         }
 
-        public Task<ServiceResult> DeleteByGuidAsync(Guid? id)
+        public async Task<ServiceResult> DeleteByGuidAsync(Guid? id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var tarjeta = await _repository.GetByGuidAsync(id, "IdTarjeta", t => t.Cliente, t => t.Descuentos);
+                if (tarjeta == null)
+                {
+                    return ServiceResult.FailureResult("Tarjeta no encontrada");
+                }
+
+                await _repository.DeleteByGuidAsync(id);
+                await _repository.SaveAsync();
+
+                return ServiceResult.SuccessResult("Tarjeta eliminada con éxito");
+            }
+            catch (Exception e)
+            {
+                return ServiceResult.FailureResult(e.Message);
+            }
         }
 
-        public Task<ServiceResult<IEnumerable<TarjetaFidelidad>>> GetAllAsync()
+        public async Task<ServiceResult<IEnumerable<TarjetaFidelidad>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var tarjetas = await _repository.GetAllAsync();
+                return ServiceResult<IEnumerable<TarjetaFidelidad>>.SuccessResult(tarjetas);
+            }
+            catch (Exception e)
+            {
+                return (ServiceResult<IEnumerable<TarjetaFidelidad>>)ServiceResult.FailureResult(e.Message);
+            }
         }
 
-        public Task<ServiceResult<TarjetaFidelidad>> GetByGuidAsync(Guid? id)
+        public async Task<ServiceResult<TarjetaFidelidad>> GetByGuidAsync(Guid? id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var tarjeta = await _repository.GetByGuidAsync(id, "IdTarjeta", t => t.Cliente, t => t.Descuentos);
+                if (tarjeta == null)
+                {
+                    return ServiceResult<TarjetaFidelidad>.FailureResult("Tarjeta no encontrada");
+                }
+
+                return ServiceResult<TarjetaFidelidad>.SuccessResult(tarjeta);
+            }
+            catch (Exception e)
+            {
+
+                return (ServiceResult<TarjetaFidelidad>)ServiceResult.FailureResult(e.Message);
+            }
         }
 
         public Task<ServiceResult<TarjetaFidelidad>> GetByIdAsync(int? id)
@@ -37,9 +96,52 @@ namespace SistemaDeFidelidad.Services
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResult> UpdateAsync(TarjetaFidelidad entity)
+        public async Task<ServiceResult> UpdateAsync(TarjetaFidelidad entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if(entity.Puntos > 0 && entity.Puntos <= 1)
+                {
+                    return ServiceResult.FailureResult("Solo se puede asignar un punto a la vez, y debe ser mayor que 0 e igual a 1");
+                }
+                
+
+                var tarjeta = await _repository.GetByGuidAsync(entity.IdTarjeta, "IdTarjeta", t => t.Cliente, t => t.Descuentos);
+                if (tarjeta == null)
+                {
+                    return ServiceResult.FailureResult("Tarjeta no encontrada");
+                }
+
+                //Puntos mayores a 5 se registra 25% de descuento
+                if (tarjeta.Puntos == 5)
+                {
+                    var descuento = new DescuentosCliente
+                    {
+                        IdCliente = tarjeta.IdCliente,
+                        IdTarjeta = tarjeta.IdTarjeta,
+                        CantidadDescuento = 25,
+                        Usado = false,
+
+                    };
+
+                    await _descuentosCliente.AddAsync(descuento);
+                    await _descuentosCliente.SaveAsync();
+                }
+
+
+                tarjeta.Puntos = entity.Puntos;
+                tarjeta.Activa = entity.Activa;
+
+                await _repository.UpdateAsync(tarjeta);
+                await _repository.SaveAsync();
+
+                return ServiceResult.SuccessResult("Tarjeta actualizada con éxito");
+
+            }
+            catch (Exception e)
+            {
+                return ServiceResult.FailureResult(e.Message);
+            }
         }
     }
 }
