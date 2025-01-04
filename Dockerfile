c3ed166cecec461027bc202876d90245ba7b82db
Pablo Palacios <72522406+PabloPalaciosDev@users.noreply.github.com>
2025-01-03 22:08:19 -0500

    - ADD model de eventos - UPDATE ubicación de dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿# Imagen base de ASP.NET Core runtime
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+WORKDIR /app
+EXPOSE 8080
+
+# Imagen base de SDK para compilar la aplicación
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY ["SistemaDeFidelidad.csproj", "./"]
+RUN dotnet restore "SistemaDeFidelidad.csproj"
+COPY . .
+RUN dotnet publish -c Release -o /app
+
+# Copia los archivos compilados a la imagen final
+FROM base AS final
+WORKDIR /app
+COPY --from=build /app .
+ENTRYPOINT ["dotnet", "SistemaDeFidelidad.dll"]
---
+++ SistemaDeFidelidad/Models/Eventos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SistemaDeFidelidad.Models
+{
+    public class Eventos
+    {
+        //id evento guid
+        [Key]
+        public Guid IdEvento { get; set; }
+        [JsonIgnore]
+        [ForeignKey("IdCliente")]
+        public ClienteParticipante Cliente { get; set; } = null!;
+
+        public DateTime fechaEvento { get; set; }
+        public string nombreEvento { get; set; } = string.Empty;
+        public string horaEvento { get; set; } = string.Empty;
+        public string lugarEvento { get; set; } = string.Empty;
+
+    }
+}
